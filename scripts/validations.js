89931b9c9487b16905f497153bd20022585ae09a
Brad Masciotra <63364775+bradmasy@users.noreply.github.com>
2022-05-04 16:37:19 -0700

    updated regex for validations

--- scripts/validations.js
+++ scripts/validations.js
@@ -7,6 +7,7 @@
  */
 
 const EMPTY_INPUT = 0;
+
 const EXCEEDED_INPUT = 20
 
 function validatePassword(password) {
@@ -53,29 +54,67 @@ function validateEmail(emailAddress){
     return valid
 }
 
-validateEmail("1@hotmail.ca");
+// validateEmail("1@hotmail.ca");
 
 
 function confirmPassword(password, confirmPassword)
 {
-    let valid = true;
+    let valid             = true;
+    let expression_lower  = /[a-z]+/;
+    let expression_upper  = /[A-Z]+/;
+    let expression_number = /[0-9]+/;
+    let expression_symbol = /[`~!@#$%^&*()_+<>?,./;':"[\]{}]+/;
 
-    if (confirmPassword === EMPTY_INPUT)
-    {
-        valid = false;
-        console.log("No Input found")
+    let lower  = expression_lower.exec(password);
+    let upper  = expression_upper.exec(password);
+    let number = expression_number.exec(password);
+    let symbol = expression_symbol.exec(password);
+
+
+    if(lower){
+        console.log("valid");
     }
 
-    if (password !== confirmPassword)
-    {
-        valid = false;
-        console.log("Passwords do not match")
-    }
+    // if (lower === null)
+    // {
+    //     valid = false;
+    //     console.log("No lowercase characters found")
+    // }
+
+    // if (upper === null)
+    // {
+    //     valid = false;
+    //     console.log("No uppercase characters found")
+    // }
+
+    // if (number === null)
+    // {
+    //     valid = false;
+    //     console.log("No number characters found")
+    // }
+
+    // if (symbol === null)
+    // {
+    //     valid = false;
+    //     console.log("No symboe characters found")
+    // }
+
+    // if (confirmPassword === EMPTY_INPUT)
+    // {
+    //     valid = false;
+    //     console.log("No Input found")
+    // }
+
+    // if (password !== confirmPassword)
+    // {
+    //     valid = false;
+    //     console.log("Passwords do not match")
+    // }
 
     return valid
 }
 
-
+confirmPassword("HELLO","Hello");
 
 // test expressions
 
