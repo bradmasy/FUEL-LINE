6142a161396cf1cc46ec727de109e2326a70edf8
Carson.O <carcango@gmail.com>
2022-05-06 13:06:27 -0700

    coded for account creation with the database

--- public/scripts/signup.js
+++ public/scripts/signup.js
@@ -2,17 +2,15 @@
  * signup.JS for the signup page
  */
 
-
 /**
  * Constant Variables
  */
 
-const $backButton        = $("#back-button");
-const $confirmButton     = $("#confirm-button");
-const $userPassword      = $("#user-pass");
-const $showPasswordBox   = $("#show");
+const $backButton = $("#back-button");
+const $confirmButton = $("#confirm-button");
+const $userPassword = $("#user-pass");
+const $showPasswordBox = $("#show");
 const $showPasswordTitle = $("#pass-title");
-
 
 /**
  * Let Variables
@@ -29,42 +27,79 @@ let passwordVisible = false; // for revealing the password.
  */
 
 $backButton.on("click", () => {
-    window.location.href = "../index.html"
-})
+  window.location.href = "../index.html";
+});
 
-$confirmButton.on("click", ()=> {
-    // alert("confirm");
-    console.log("hi");
-})
+$confirmButton.on("click", () => {
+  // alert("confirm");
+  console.log("hi");
+});
 
 /**
  * Turns function handlers on for show password.
  */
-function showPassword()
-{
-    $showPasswordBox.on("click",() => {
-
-        if(!passwordVisible)
-        {
-            $showPasswordTitle.html("Hide Password");
-            $userPassword.attr("type","text");
-            passwordVisible = true;
-        }
-        else
-        {
-            $showPasswordTitle.html("Show Password");
-            $userPassword.attr("type","password");
-            passwordVisible = false;
-        }
-    })
+function showPassword() {
+  $showPasswordBox.on("click", () => {
+    if (!passwordVisible) {
+      $showPasswordTitle.html("Hide Password");
+      $userPassword.attr("type", "text");
+      passwordVisible = true;
+    } else {
+      $showPasswordTitle.html("Show Password");
+      $userPassword.attr("type", "password");
+      passwordVisible = false;
+    }
+  });
 }
 
+function proceedToHome() {
+  console.log("successful Signup");
+  window.location.href = "/success.html";
+}
+
+function checkUserExists(data) {
+  if (data.length === 0) {
+    console.log("User not found!");
+    alert("User not found");
+  } else {
+    proceedToHome();
+  }
+}
+
+function attemptSignup() {
+  console.log("attemptSignup" + "got called!");
+  console.log($("#username").val());
+  console.log($("#email").val());
+  console.log($("#password1").val());
+  console.log($("#password2").val());
+
+  adminIsChecked = false;
+  if ($("#admin-status").is(":checked")) adminIsChecked = true;
 
 
-function setup()
-{
-    console.log("document ready");
-    showPassword();
+  if ($("#password1").val() === $("#password2").val()) {
+    $.ajax({
+      url: "http://localhost:5000/attemptSignup",
+      //   url: "https://radiant-anchorage-93970.herokuapp.com/findUnicornByWeight",
+      type: "POST",
+      data: {
+        username: $("#username").val(),
+        email: $("#email").val(),
+        password: $("#password1").val(),
+        admin: adminIsChecked
+      },
+      success: checkUserExists,
+    });
+    // resetPage();
+  } else {
+    alert("passwords do not match");
+  }
+}
+
+function setup() {
+  console.log("document ready");
+  showPassword();
+  $confirmButton.on("click", attemptSignup);
 }
 
 $(document).ready(setup);
