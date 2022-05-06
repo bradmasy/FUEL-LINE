83af76d3709764c2de74e4638329622d2ef68865
Carson.O <carcango@gmail.com>
2022-05-06 12:09:55 -0700

    fixed login screening to block users not found

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,46 +1,18 @@
-<<<<<<< HEAD
-/**
- * Login.js
- * Login.js
- */
 
-/*
- * Variables
- */
-
-let passwordVisible = false;
-
-/**
- * Displays submit message on successful login.
- * 
- * @param {Event} data 
- */
-function proceedToHome(data) {
-  console.log("successful login")
+function proceedToHome() {
+  console.log("successful login");
+  window.location.href = "/success.html";
 }
 
-/**
- * Toggles the password visibility for the user.
- */
-function showPassword() {
-  $("#show-pass-box").on("click", () => {
-
-    if (!passwordVisible) {
-      $("#pass-label").html("Hide Password");
-      $("#password_log").attr("type", "text");
-      passwordVisible = true;
-    }
-    else {
-      $("#pass-label").html("Show Password");
-      $("#password_log").attr("type", "password");
-      passwordVisible = false;
-    }
-  })
+function checkUserExists(data) {
+  if (data.length === 0) {
+    console.log("User not found!");
+    alert("User not found");
+  } else {
+    proceedToHome();
+  }
 }
 
-/**
- * 
- */
 function attemptLogin() {
   console.log("attemptLogin" + "got called!");
   console.log($("#username_log").val());
@@ -53,59 +25,14 @@ function attemptLogin() {
       username: $("#username_log").val(),
       password: $("#password_log").val(),
     },
-    success: proceedToHome,
+    success: checkUserExists,
   });
   // resetPage();
 }
 
-
-
 function setup() {
-  console.log("login.js loaded")
+  console.log("login.js loaded");
   $("#submit-button").on("click", attemptLogin);
-  showPassword();
-
 }
 
 $(document).ready(setup);
-=======
-
-let isAdmin = false;
-
-function proceedToHome(data){
-    console.log(data)
-    console.log("successful login")
-    if (isAdmin) {
-      window.location.href ="/admin_user_views.html"
-    } else {
-      window.location.href ="/success.html"
-    }
-}
-
-
-function attemptLogin() {
-    console.log("attemptLogin" + "got called!"); 
-    console.log($("#username_log").val());
-    console.log($("#password_log").val());
-    $.ajax({
-      url: "http://localhost:5000/attemptLogin",
-    //   url: "https://radiant-anchorage-93970.herokuapp.com/findUnicornByWeight",
-      type: "POST",
-      data: {
-        username: $("#username_log").val(),
-        password: $("#password_log").val(),
-      },
-      success: proceedToHome,
-    });
-    // resetPage();
-  }
-
-
-function setup() {
-    console.log("login.js loaded")
-    $("#submit-button").on("click", attemptLogin);
- 
-  }
-  
-  $(document).ready(setup);
->>>>>>> c71522f472c7d14488bd66e27c60fcab751d80e6
