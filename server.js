1de265cff717827a94c0c7374958e9f387ad3620
Brad Masciotra <63364775+bradmasy@users.noreply.github.com>
2022-05-06 11:26:02 -0700

    working on sessions

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 // homepage
 // executed first when the serve is initiated
 
-const express = require("express");
-const app = express();
+const express    = require("express");
+const app        = express();
+const https      = require("https");
+const bodyparser = require("body-parser");
+const mongoose   = require("mongoose");
+const session    = require("express-session");
+
 app.set("view engine", "ejs");
-const https = require("https");
 
 app.listen(process.env.PORT || 5000, function (err) {
   if (err) console.log(err);
 });
 
-const bodyparser = require("body-parser");
 app.use(
   bodyparser.urlencoded({
     extended: true,
   })
 );
 
-const mongoose = require("mongoose");
+app.use(session({secret:"shhhh", saveUninitialized:true, resave:true}));
 
 mongoose.connect(
   "mongodb+srv://fuel_line_2022:fuel@cluster0.vcuj9.mongodb.net/FuelLineDTC12?retryWrites=true&w=majority",
@@ -29,17 +32,22 @@ mongoose.connect(
 
 //  mongoose.connect("mongodb://localhost:27017/Fuel_Line"); // our database on local host, not yet on server...
 
-const userSchema = new mongoose.Schema({
-  username: String,
-  passwordt: String,
-});
-const userModel = mongoose.model("users", userSchema);
 
+
+  const userSchema = new mongoose.Schema({
+    username: String,
+    passwordt: String,
+  });
+  const userModel = mongoose.model("users", userSchema);
+ 
 /*------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 app.get("/login", function (req, res) {
   res.sendFile(__dirname + "/public/login.html");
 });
+
+
+
 
 // app.get("/profile/:id", function (req, res) {
 //   // console.log(req);
@@ -90,12 +98,16 @@ app.post("/attemptLogin", function (req, res) {
       } else {
         console.log("Data " + users);
       }
+      console.log(users);
       res.send(users);
+      res.render()
     }
   );
 });
 
 console.log("Server Running");
+
+
 
 /*------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
@@ -103,9 +115,11 @@ console.log("Server Running");
  * Database Connection
  */
 
-app.post("/displayUsersToAdmin", function (req, res) {
+ app.post("/displayUsersToAdmin", function (req, res) {
   console.log("req. has been recieved");
-  userModel.find({}, function (err, users) {
+  console.log(req.body.username);
+
+  userModel.find({ name: req.body.username }, function (err, users) {
     if (err) {
       console.log("Error " + err);
     } else {
