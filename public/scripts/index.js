f9751e030df804d9d429f67382770d36c5a2937f
Brad Masciotra <63364775+bradmasy@users.noreply.github.com>
2022-05-09 16:44:54 -0700

    making profile page

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,57 +1,28 @@
-// const express    = require("express");
-// const app        = express();
-// const mongoose   = require("mongoose");
-// const bodyParser = require("body-parser");
-// let schema       = mongoose.schema;
-// let USER         = require("./userModel");
-// const { $where } = require("./userModel");
+const $profileButton = $("#profile");
+const $signupButton  = $("#signup");
+const $homeButton    = $("#home-button");
 
-// const { createPublicKey } = require("crypto");
+console.log($profileButton);
 
-// app.set("view engine","ejs");
-// app.use(bodyParser.urlencoded({extended:true}));
-// app.use(express.static("public"));
-
-
-// mongoose.connect("mongodb://localhost:27017/Fuel_Line");
-
-// let db = mongoose.connection;
-
-// db.once("open",function(){
-//     console.log("connection successful");
-// })
-
-
-// let user = new USER({username:"ken",password:"password"});
-
-// user.save(function(err){
-//     if(err){
-//         console.log(err);
-//     }
-//     else{
-//         console.log("user is saved");
-//     }
-// })
-
-
-
-const $signupButton = $("#signup");
-const $homeButton = $("#home-button");
-
-$signupButton.mouseleave(function(){
+// $signupButton.mouseleave(function(){
   
-})
+// })
 
-$signupButton.click( function(){
-    // window.location.href = "../signup.html"
-})
+// $signupButton.click( function(){
+//     // window.location.href = "../signup.html"
+// })
 
 $homeButton.on("click", function(){
-    window.location.href = "../index.html"
+    window.location.href = "/index"
 })
 
+$("#profile").on("click",() =>{
+    console.log("clicked");
+    // window.location.href = "/profile";
 
-console.log($signupButton);
+    window.location.href = "/profile.html";
+})
+
 
 function setup()
 {
