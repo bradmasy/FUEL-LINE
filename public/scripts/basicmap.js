fd3ff8f53ac3a297faa86aae477a7adc890e76ee
Ethan Johnston <ethan_johnston13@hotmail.com>
2022-05-12 16:23:42 -0700

    remove uneeded buttons from map

--- public/scripts/basicmap.js
+++ public/scripts/basicmap.js
@@ -4,6 +4,9 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map-section"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
+    mapTypeControl: false,
+    scaleControl: false,
+    zoomControl: true,
   });
 }
 
