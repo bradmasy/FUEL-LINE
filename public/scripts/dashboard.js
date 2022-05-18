a492fd4ba0979c82d1c36deae5ffc7b4f7b52a11
Yuen <garymo@hotmail.ca>
2022-05-18 11:08:04 -0700

    debug nav bar color

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -69,7 +69,18 @@ function setup()
     getUserData();
 }
 
+let $topBars =  $(".top-bar");
+    
+for(let i = 0; i < 4; i++)
+{
+    let $element = $($topBars[i]);
+    if(i == 0)
+    {
+        $element.css("background-color","#FF912C");
 
+    }
+    console.log($element);
+}
 
 
 
